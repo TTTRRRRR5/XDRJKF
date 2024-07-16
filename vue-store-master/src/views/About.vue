325ce470d36a8731849dedbcc341743bf2e93b01
TTTRRRRR5 <162893339+TTTRRRRR5@users.noreply.github.com>
2024-07-16 16:56:28 +0800

    Add files via upload

---
+++ vue-store-master/src/views/About.vue
@@ -0,0 +1,54 @@
+
+<template>
+  <div class="about" id="about" name="about">
+    <div class="about-header">
+      <div class="about-title">
+        <i class="el-icon-tickets" style="color: #ff6700;"></i>
+        关于我们
+      </div>
+    </div>
+    <div class="about-content">
+      求。通过模仿小米商城的实现，项目旨在提供一个前后端分离、功能完整的电商平台，满足用户的基本购物需求。本项目实现了电商平台的基本功能，包括用户登录注册、商品展示、商品详情查看、购物车管理、订单结算、用户收藏和订单管理等。
+      作者：曲艺、杨庭瑞、施圣泽、王成纬
+    </div>
+  </div>
+</template>
+<script>
+// import MyMarkdown from "../components/MyMarkdown";
+// export default {
+//   components: {
+//     MyMarkdown
+//   }
+// };
+</script>
+<style scoped>
+.about {
+  background-color: #f5f5f5;
+}
+.about .about-header {
+  height: 64px;
+  background-color: #fff;
+  border-bottom: 2px solid #ff6700;
+}
+.about .about-header .about-title {
+  width: 1225px;
+  margin: 0 auto;
+  height: 64px;
+  line-height: 64px;
+  font-size: 28px;
+}
+.about .content {
+  padding: 20px 0;
+  width: 1225px;
+  margin: 0 auto;
+}
+.about .content .goods-list {
+  margin-left: -13.7px;
+  overflow: hidden;
+}
+.about .about-content {
+  width: 1225px;
+  margin: 0 auto;
+  background-color: #fff;
+}
+</style>
